1e1fe3d54023ef0cd39d4a2d65f077ace00a7c37
jayme luis <jaymeluis23@gmail.com>
2026-03-23 08:12:40 -0300

    conexão com o banco

---
+++ main.py
@@ -0,0 +1,12 @@
+# conexão com o bnaco 
+
+import mysql.connector 
+
+conexão = mysql.connector.connect(
+    host= "10.30.29.162",
+    port= 3309,
+    user= "root",
+    password= "root123"
+
+)
+
